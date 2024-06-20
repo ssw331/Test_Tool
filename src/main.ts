a8d4432783f224a33ce58490372544eb538b9e16
KingofField <1092888175@qq.com>
2024-06-21 00:17:16 +0800

    修改JSON获取

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,23 @@ import router from './router/index'
 createApp(App).use(Antd).use(router).mount('#app')
 
 import axios from 'axios'
-interface TestResult
+interface AssertionResult {
+    "ancestorTitles": string[],
+    "fullName": string,
+    "status": string,
+    "title": string,
+    "duration": number,
+    "failureMessages": string[]
+}
+interface TestResult {
+    "assertionResults": AssertionResult[],
+    "startTime": number,
+    "endTime": number,
+    "status": string,
+    "message": string,
+    "name": string
+}
+interface Result
 {
     "numTotalTestSuites": number,
     "numPassedTestSuites": number,
@@ -20,28 +36,65 @@ interface TestResult
     "numTodoTests": number,
     "startTime": number,
     "success": boolean,
-    "testResults":
-        {
-            "assertionResults":
-                {
-                    "ancestorTitles": string[],
-                    "fullName": string,
-                    "status": string,
-                    "title": string,
-                    "duration": number,
-                    "failureMessages": string[]
-                }[]
-            ,
-            "startTime": number,
-            "endTime": number,
-            "status": string,
-            "message": string,
-            "name": string
-        }[]
+    "testResults": TestResult[]
 }
-const JsonParser = async (url: string): Promise<TestResult> => {
-    return await axios.get(url).then(r => {
-        return r.data
-    })
+interface Column{
+    title:string,
+    key:string,
+    dataIndex?:string
+}
+const Columns:Column[]=[
+    {
+        title:"title",
+        key:"title",
+        dataIndex:"title"
+    },
+    {
+        title:"status",
+        key:"status",
+        dataIndex:"status"
+    },
+    {
+        title:"duration",
+        key:"duration",
+        dataIndex:"duration"
+    },
+    {
+        title:"failureMessages",
+        key:"failureMessages",
+        dataIndex:"failureMessages"
+    }
+]
+interface Data{
+    key:string,
+    title:string,
+    status:string,
+    duration:number,
+    failureMessages:string[]
+}
+class TableData{
+    columns = Columns;
+    data:Data[]=[]
+}
+const JsonParser = async (url: string): Promise<TableData> => {
+    const r:Result =  (await axios.get(url)).data
+    const testResults=r.testResults
+    let ret=new TableData()
+    for(let i=1;i<=testResults.length;i=i+1)
+    {
+        let assertionResults = testResults[i-1].assertionResults
+        for(let j:number=1;j<=assertionResults.length;j=j+1)
+        {
+            let assertionResult = assertionResults[j-1]
+            ret.data.push({
+                key: String(i),
+                title: assertionResult.title,
+                status: assertionResult.status,
+                duration: assertionResult.duration,
+                failureMessages: assertionResult.failureMessages
+            })
+        }
+    }
+    return ret
 }
 console.log(await JsonParser("/src/js-report.json"))
