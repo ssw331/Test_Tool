acd1965fd733d290414b0d89808893aa086133e6
KingofField <1092888175@qq.com>
2024-06-20 23:28:53 +0800

    添加json获取

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,43 @@ import Antd from 'ant-design-vue'
 import router from './router/index'
 
 createApp(App).use(Antd).use(router).mount('#app')
+
+import axios from 'axios'
+interface TestResult
+{
+    "numTotalTestSuites": number,
+    "numPassedTestSuites": number,
+    "numFailedTestSuites": number,
+    "numPendingTestSuites": number,
+    "numTotalTests": number,
+    "numPassedTests": number,
+    "numFailedTests": number,
+    "numPendingTests": number,
+    "numTodoTests": number,
+    "startTime": number,
+    "success": boolean,
+    "testResults":
+        {
+            "assertionResults":
+                {
+                    "ancestorTitles": string[],
+                    "fullName": string,
+                    "status": string,
+                    "title": string,
+                    "duration": number,
+                    "failureMessages": string[]
+                }[]
+            ,
+            "startTime": number,
+            "endTime": number,
+            "status": string,
+            "message": string,
+            "name": string
+        }[]
+}
+const JsonParser = async (url: string): Promise<TestResult> => {
+    return await axios.get(url).then(r => {
+        return r.data
+    })
+}
+console.log(await JsonParser("/src/js-report.json"))
